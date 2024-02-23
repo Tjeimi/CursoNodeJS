55e850035fda4cac90a33dc1c44dacda0fcd3904
vagnervog <vagnervogel@gmail.com>
2024-02-23 09:17:15 -0300

    reorganização exercicios

--- index.js
+++ index.js
@@ -1,137 +1,233 @@
+//                     exercicio 1 (aula 3)
 /*
 var nome = "Vagner"
 var sobrenome = "Vogel"
 var idade = 42
 var estudante = true
-console.log("Olá, me chamo " , nome , sobrenome , " e tenho " , idade , " anos de idade ");
+    console.log("Olá, me chamo " , nome , sobrenome , " e tenho " , idade , " anos de idade ");
 */
+//                     exercicio 2 (aula 3)
 /*
 var nome = prompt("Digite o seu nome");
 var idade = prompt("Qual a sua idade?");
-console.log (typeof nome)
-console.log(typeof idade)
+    console.log (typeof nome)
+    console.log(typeof idade)
 */
+//                    OPERADORES ARITMÉTICOS
 
-// OPERADORES ARITMÉTICOS
-/*    SOMA
+/*                            SOMA
 const primeiroValor = 10
 const segundoValor = 20
 const resultado = primeiroValor + segundoValor + 4
-console.log(resultado) //34
+    console.log(resultado) //34
 
-//    SUBTRAÇÃO
+//                          SUBTRAÇÃO
 const valor1 = 10
 const valor2 = 20
 const valor3 = valor2 - valor1
-console.log(valor3) // 10
+    console.log(valor3) // 10
 */
 /*
-//    MULTIPLICAÇÃO
+//                         MULTIPLICAÇÃO
 const valorA = 10
 const valorB = 25
 const valorC = valorB * valorA
-console.log(valorC)  // 250
+    console.log(valorC)  // 250
 */
 /*
-//    DIVISÃO
+//                             DIVISÃO
 const valorX = 345
 const valorY = 10
 const valorZ = valorX / valorY
-console.log(valorZ) // 34.5
+    console.log(valorZ) // 34.5
 */
 /*
-//    RESTO DA DIVISÃO
+//                         RESTO DA DIVISÃO
 const restoDaDivisao = 11 % 4
-console.log(restoDaDivisao) // 3
+    console.log(restoDaDivisao) // 3
 */
 /*
-//  >>> EXERCICIO 1 <<<
+//                        exercicio 1 (aula 4)
 // A)
 var a = 3
 var b = 4
 var c = a + b
-console.log(c) // 7
+    console.log(c) // 7
 */
 /*
 // B)
 var valorA = 3
 var valorB = 5
 var valorC = valorA * valorB
-console.log(valorC / 2) // 7.5
+    console.log(valorC / 2) // 7.5
 */
 /*
 // C)
 var valor1 = 5
 var valor2 = 4
 var valor3 = valor1 - valor2
-console.log(valor3 * -1) // -1
+    console.log(valor3 * -1) // -1
 */
 /*
 // D)
 var restoDivisao = 234 % 5
-console.log(restoDivisao) // 4
+    console.log(restoDivisao) // 4
 */
 /*
-//   >>> EXERCICIO 2 <<<
+//                       exercicio 2 (aula 4)
 // A)
 var a = 2
 var b = 3
-console.log(a === b) // false
+    console.log(a === b) // false
 */
 /*
 // B)
 var valorA = 4
 var valorB = 3
-console.log(valorA !== valorB) // true
+    console.log(valorA !== valorB) // true
 */
 /*
-// C
+// C)
 var num1 = 5
 var num2 = 2
-console.log(num1 > num2) // true
+    console.log(num1 > num2) // true
 */
 /*
-// D
+// D)
 var numA = 2
 var numB = 9
-console.log(numA < numB) // true
+    console.log(numA < numB) // true
 */
-//   >>> EXERCICIO 3 <<<
+//                       exercicio 3 (aula 4)
 /*
 var a = true
 var b = false
 var c = true
-console.log(a && b); // false
-console.log(b && c); // false
-console.log(a && c); // true
-console.log(a && b && c); // false
+    console.log(a && b); // false
+    console.log(b && c); // false
+    console.log(a && c); // true
+    console.log(a && b && c); // false
 */
 /*
-//   >>> EXERCICIO 4 <<<
+//                       exercicio 4 (aula 4)
 var a = true
 var b = false
 var c = true
-console.log(a || b); // true
-console.log(b || c); // true
-console.log(a || c); // true
-console.log(a || b || c) // true
+    console.log(a || b); // true
+    console.log(b || c); // true
+    console.log(a || c); // true
+    console.log(a || b || c) // true
 */
-//   >>> EXERCICIO 5 <<<
+/*
+//                        exercicio 5 (aula 4)
+
 var nome = prompt("Digite o seu nome");
-console.log(nome);
+    console.log(nome);
 var anoNascimento = prompt("Digite o ano em que nasceu");
-console.log(anoNascimento);
+    console.log(anoNascimento);
 var anoAtual = 2024;
 var anoFinal = 2050;
 var anoTotal = anoAtual - anoNascimento;
-console.log(`${nome}, você tem ${anoTotal} anos de idade.`)
+    console.log(`${nome}, você tem ${anoTotal} anos de idade.`)
 //console.log(anoTotal);
-console.log("É de maior? " , anoTotal >= 18);
+    console.log("É de maior? " , anoTotal >= 18);
 var anoTotal = anoFinal - anoNascimento;
-console.log(`${nome}, você terá ${anoTotal} anos de idade em ${anoFinal}.`);
+    console.log(`${nome}, você terá ${anoTotal} anos de idade em ${anoFinal}.`);
+*/
+//                        exercicio 1 (aula 5)
+/*      >> exemplo com concatenação <<
+const nome = prompt("Digite o seu nome:")
+const cor = prompt("Qual a sua cor preferida?")
+    console.log("A cor favorita do " + nome + " é " + cor)
+*/
+/*      >> exemplo com template strings <<
+const nome = prompt("Digite o seu nome:")
+const cor = prompt("Qual a sua cor favorita?")
+    console.log(`A cor favorita do ${nome} é ${cor}`)
+*/
+//                        exercicio 2 (aula 5)
+/*
+const frase = prompt("Digite uma frase:");
+const fraseMaiuscula = frase.toUpperCase();
+const linguaI = frase.replaceAll("o", "i");
+    console.log(fraseMaiuscula);
+    console.log(linguaI);
+    console.log(frase.length);
+*/
+//                        exercicio 3 (aula 5)
+/*
+const racaCachorro = ["SRD", "Labrador", "Collie", "Pitbull", "Dobermann"];
+//const num = prompt("Digite um número de 0 a 4:");
+//console.log(racaCachorro[num]);
+    console.log(racaCachorro[prompt("Digite um numero de 0 a 4:")]);
+*/
+//                        exercicio 4 (aula 5)
+/*
+const numeros = [1, 2, 3, 4, 5, 6];
+    console.log(numeros.length);
+numeros.push(7);
+    console.log(numeros);
+numeros.splice(3, 2);
+    console.log(numeros);
+    console.log(numeros.length);
+*/
 
+//                        exercicio 1 (aula 6)
+/*
+function imprima(){
+    console.log("Olá mundo!");
+}
+imprima();
+*/
+//                        exercicio 2 (aula 6)
+/*
+function nomeA(nome){
+   console.log(`Olá ${nome}`);
+}
+nomeA('Raul');
+nomeA('José');
+nomeA('Adão');
+*/
+//                        exercicio 3 (aula 6)
+/* 
+function nums(num1, num2){
+    return num1 + num2   
+}
+var numsTotal= nums(2, 3) // 5
+    console.log(numsTotal)
 
+/*                             
+function areaCalculada(comprimento, peso){
+    return comprimento + peso
+}
+var tamanhoTotal = areaCalculada(4, 6)
+    console.log(tamanhoTotal) // 10
+*/
+/*
+//                        exercicio 4 (aula 6)
 
+function numeros(array){
+    return [(array[8])/2, (array[0])/2];  // <<<<<<<<< revisar
+}
+    console.log(numeros([1,2,3,4,5,6,7,8,12])) // [4.5, 0.5]
+*/
+/*
 
+//                    >> EXPRESSÕES DE FUNÇÃO <<
+//                       exercicio 5 (aula 6)
 
+//               ** SINTAXE DE EXPRESSÃO DE FUNÇÃO **
 
+const names = function(name1, name2, name3){
+    return "Olá "  + name1 + "\n, Olá " + name2 +"\n, Olá " + name3;
+}
+    console.log(names('vagner', 'jose', 'maria'));
+*/
+/*
+//               ** SINTAXE DE ARROW FUNCTION **
+
+let somaNumbers = (num1, num2) => {
+    return num1 + num2
+}
+    console.log(somaNumbers(2, 3));
+*/
